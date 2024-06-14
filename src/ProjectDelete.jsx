451272a8d16a25d53993a6e769a2082f49556047
jordanoboyle <jmcmcode@gmail.com>
2024-06-14 15:14:47 -0600

    Project Update and Delete separated

---
+++ src/ProjectDelete.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export function ProjectDelete () {
+
+  // const handleDeleteProject = () => {
+  //   console.log("submitting delete");
+  //   axios.delete("http://localhost:3000/projects/{id}.json")
+  // };
+
+  return(
+    <div>
+      Render within the ProjectShow Modal
+      <button> Delete This Project</button>
+    </div>
+  );
+}
---
+++ src/ProjectUpdate.jsx
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export function ProjectUpdate () {
+
+  return(
+    <div>
+      Render this within ProjectShow modal
+    </div>
+  )
+}
