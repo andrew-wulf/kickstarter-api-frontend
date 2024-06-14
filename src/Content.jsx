c3c7020bee11841c2fbb5b6a812fa335ebf2c65b
bradzara <bradzara93@gmail.com>
2024-06-14 10:55:57 -0700

    Add show action with modal button

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,15 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { ProjectsIndex } from './ProjectsIndex'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { ProjectsIndex } from './ProjectsIndex';
+import { ProjectsShow } from './ProjectsShow';
+import { Modal } from './Modal';
 
 export function Content() {
 
-
-  const [projects, setProjects] = useState({})
+  
+  const [projects, setProjects] = useState({});
+  const [isProjectsShowVisible, setIsProjectsShowVisible] = useState(false);
+  const [currentProject, setCurrentProject] = useState({});
 
   const projectsIndex = () => {
     axios.get('http://localhost:3000/projects.json')
@@ -17,16 +21,29 @@ export function Content() {
       console.log(error);
     })
   }
+  
+  const handleShowProject = (project) => {
+    console.log("handleShowProject", project);
+    setIsProjectsShowVisible(true);
+    setCurrentProject(project);
+  }
 
-  useEffect(projectsIndex, [])
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsProjectsShowVisible(false);
+  };
+  
+  useEffect(projectsIndex, []);
 
 
 
 
   return (
     <div className="content">
-      <ProjectsIndex data={projects}/>
+      <ProjectsIndex data={projects} onShowProject={handleShowProject} />
+      <Modal show={isProjectsShowVisible} onClose={handleClose}>
+        <ProjectsShow project={currentProject} />
+      </Modal>
     </div>
-
-  )
+  );
 }
