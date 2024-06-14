6b4afd4e3426180ed698c5b341474e28ecbf8693
jordanoboyle <jmcmcode@gmail.com>
2024-06-14 16:33:52 -0600

    no-function Update form, Will finish, need adjustments to Rails

--- src/ProjectUpdate.jsx
+++ src/ProjectUpdate.jsx
@@ -5,6 +5,29 @@ export function ProjectUpdate () {
   return(
     <div>
       Render this within ProjectShow modal
+      <form>
+        <label>
+          Update Title:
+          <input type="text" name="title" />
+        </label>
+        <br/>
+        <label> 
+          Update Descript
+          <input type="text" name="description"/>
+        </label>
+        <br/>
+        <label>
+          Goal Target Update:
+          <input type="integer" name="goal_amount" />
+        </label>
+        <br/>
+        <label>
+          Change Project End-Date:
+          <input type="date" name="end_date" max="2030-01-01" />
+        </label>
+        <br/>
+        <button>Submit Updates</button>
+      </form>
     </div>
   )
 }
