a8ceb39f253b476e45668aaaf6bfc601c71f4ba9
jordanoboyle <jmcmcode@gmail.com>
2024-06-14 15:07:07 -0600

    project update and delete files

---
+++ src/ProjectUpdate.jsx
@@ -0,0 +1,10 @@
+
+
+export function ProjectUpdate () {
+
+  return (
+    <div>
+      this needs to render within the show modal
+    </div>
+  )
+}
--- src/ProjectsShow.jsx
+++ src/ProjectsShow.jsx
@@ -1,3 +1,5 @@
+
+
 export function ProjectsShow(props) {
   return (
     <div>
